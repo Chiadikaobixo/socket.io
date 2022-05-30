ff94d855a68f4481887ac7777f25a785c4e60679
chiadikaobixo <chiadikaobixo@gmail.com>
2022-05-30 17:11:20 -0500

    writting to socket

--- src/index.js
+++ src/index.js
@@ -4,6 +4,45 @@ const io = require('socket.io')(9000, {
     }
 })
 
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+    users.push({userId, socketId})
+}
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId)
+}
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => {
+        user.userId !== socketId
+    })
+}
+
 io.on('connection', (socket) => {
     console.log('User connected')
+
+    // take user and socketId from user
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id)
+        io.emit('getUser', users)
+    })
+
+    // send and get message
+    socket.on('sendMessage', ({senderId, receiverId, text}) => {
+        const user = getUser(receiverId)
+        io.to(user?.socketId).emit('getMessage', {
+            senderId, text
+        })
+    })
+    
+
+    // disconnect a user
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+        removeUser(socket.id)
+        io.emit('getUser', users)
+    })
 })
